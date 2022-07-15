d72316f52212d4aa551d87395b85eaa5467acf32
eduard-ops <epustynnik@gmail.com>
2022-07-15 06:46:33 +0300

    add routes

--- homework-03/routes/api/contacts.js
+++ homework-03/routes/api/contacts.js
@@ -4,15 +4,27 @@ const router = express.Router();
 
 const ctrl = require("../../controlles/contacts");
 
-const { ctrlWrapper } = require("../../helpers");
+const { validation, ctrlWrapper } = require("../../middlewares");
+
+const { joiSchema, favoriteJoiSchema } = require("../../models/contact");
 
 router.get("/", ctrlWrapper(ctrl.listContacts));
 
 router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
 
-router.post("/", ctrlWrapper(ctrl.addContact));
+router.post("/", validation(joiSchema), ctrlWrapper(ctrl.addContact));
 
-router.put("/:contactId", ctrlWrapper(ctrl.updateContact));
+router.put(
+  "/:contactId",
+  validation(joiSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  validation(favoriteJoiSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
