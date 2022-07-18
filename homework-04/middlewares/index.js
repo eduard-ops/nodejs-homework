e1ead93e02aa104f29a39e4f085b5d71a8558cf8
eduard-ops <epustynnik@gmail.com>
2022-07-18 06:51:23 +0300

    add middlewares

--- homework-04/middlewares/index.js
+++ homework-04/middlewares/index.js
@@ -2,7 +2,10 @@ const validation = require("./validation");
 
 const ctrlWrapper = require("./ctrlWrapper");
 
+const auth = require("./auth");
+
 module.exports = {
   validation,
   ctrlWrapper,
+  auth,
 };
