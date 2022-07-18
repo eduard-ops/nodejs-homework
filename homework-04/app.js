04e50d8162ad68a79d43acb29d8b5c41fba36f7f
eduard-ops <epustynnik@gmail.com>
2022-07-18 06:48:50 +0300

    add app

--- homework-04/app.js
+++ homework-04/app.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+const authRouter = require("./routes/api/auth");
+
+const usersRouter = require("./routes/api/users");
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -16,7 +20,11 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", authRouter);
+
 app.use("/api/contacts", contactsRouter);
+
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
